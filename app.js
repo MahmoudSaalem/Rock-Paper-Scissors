b91da457d2201dff18775b43589b605d08e5b50b
MahmoudSaalem <eng.mahsalem98@gmail.com>
2019-09-16 19:32:56 +0200

    Some CSS changes

--- app.js
+++ app.js
@@ -13,11 +13,18 @@ $("#reset").on("click", function() {
 	reset();
 });
 $(".card").on("click", function() {
-	getPlayerChoice($(this)[0].id);
+	let id = $(this)[0].id;
+	getPlayerChoice(id);
 	play();
 });
 $(".card").hover(function() {
-	$(this).children("i").toggleClass("fas far");
+	let id = $(this)[0].id;
+	// $(this).children("i").toggleClass("fas far");
+	setPlayerDisplay(id);
+});
+$(".card").mouseenter(function() {
+	$("#computerDisplay").removeClass();
+	$("#computerDisplay").addClass("fas fa-question");
 });
 
 function getPlayerChoice(id) {
@@ -36,6 +43,7 @@ function getPlayerChoice(id) {
 
 function getComputerChoice() {
 	computerChoice = Math.floor(Math.random() * 3);
+	setComputerDisplay(computerChoice);
 }
 
 function play() {
@@ -43,31 +51,72 @@ function play() {
 	switch (results[playerChoice][computerChoice]) {
 		case WIN:
 			playerScore++;
-			displayStatus("win");
+			displayStatus("Win");
 			break;
 		case DRAW:
-			displayStatus("draw");
+			displayStatus("Draw");
 			break;
 		case LOSE:
 			computerScore++;
-			displayStatus("lose");
+			displayStatus("Lose");
 			break;
 	}
 	adjustScore();
 }
 
 function displayStatus(status) {
-	console.log("you " + status);
-	console.log("player " + playerChoice);
-	console.log("computer " + computerChoice);
+	let h1 = $("#status");
+	h1.text("You " + status);
+	switch (status) {
+		case "Win":
+			h1.css("color", "green");
+			break;
+		case "Draw":
+			h1.css("color", "gray");
+			break;
+		case "Lose":
+			h1.css("color", "red");
+			break;
+	}
 }
 
 function adjustScore() {
 	$("#playerScore").text(playerScore);
 	$("#computerScore").text(computerScore);
+	$(".symbol").removeClass("fas fa-crown");
+	if (playerScore > computerScore) {
+		$("#playerSymbol").addClass("fas fa-crown");
+	} else if (computerScore > playerScore) {
+		$("#computerSymbol").addClass("fas fa-crown");
+	}
 }
 function reset() {
 	playerScore = 0;
 	computerScore = 0;
+	$("#status").text("Game On");
+	$("#status").css("color", "white");
 	adjustScore();
+}
+
+function setPlayerDisplay(id) {
+	let icon = `fas fa-hand-${id}`;
+	$("#playerDisplay").removeClass();
+	$("#playerDisplay").addClass(icon);
+}
+
+function setComputerDisplay(choice) {
+	let icon = "fas fa-hand-" + getId(choice);
+	$("#computerDisplay").removeClass();
+	$("#computerDisplay").addClass(icon);
+}
+
+function getId(choice) {
+	switch (choice) {
+		case ROCK:
+			return "rock";
+		case PAPER:
+			return "paper";
+		case SCISSORS:
+			return "scissors";
+	}
 }
